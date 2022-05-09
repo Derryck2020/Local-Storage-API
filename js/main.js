aa31302f44a88a98ff385efc63cd802640c25148
Derryck <oseiagyemanb@gmail.com>
2022-05-09 03:44:50 +0100

    Second commit

--- js/main.js
+++ js/main.js
@@ -2,8 +2,9 @@
 
 if (!localStorage.getItem('machineScore')){  // This conditional was in the function addAnotherOne, but we made it a global,
     localStorage.setItem('machineScore', 0)  // so that each time the function is called, it will reset the machineScore to 0.
-                                         // On page refresh, localStorage is set to 0, we will get null if we try to getItem which is not in localStorage.
-                                         // Null is a falsy value. So if we do not get a falsy value "!", then upon page refresh, we wont get zero for machineScore.
+                                             // On page refresh, localStorage is set to 0, we will get null if we try to getItem which is not in localStorage.
+                                             // Null is a falsy value. So if we do not get a falsy value "!", then upon page refresh, we wont get zero for machineScore.
+                                             // But if we ignore the "!", each time the page refreshes, machineScore will reset to 0.
 }
 
 document.querySelector('button').addEventListener('click', addAnotherOne)
